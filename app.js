ca15f4e335fe65140e302c37cf430161b2fcb9e1
Junior Turino <alvi.turino@gmail.com>
2025-09-23 19:40:53 -0300

    Adicionei uma página só com projetos em Python

--- app.js
+++ app.js
@@ -1,4 +1,10 @@
+// Menu Hamburger Toggle
 const hamburger = document.querySelector(".hamburger");
 const nav = document.querySelector(".nav");
 
 hamburger.addEventListener("click", () => nav.classList.toggle("active"));
+
+
+// Fechar o menu ao clicar em um link
+const fecharMenu = () => nav.classList.remove("active");
+document.getElementById("sobre").addEventListener("click", fecharMenu);
